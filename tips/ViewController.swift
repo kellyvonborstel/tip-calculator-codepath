86bb65cf490ee050687433d818ea9dad655448b7
Kelly von Borstel <kellyvonborstel@gmail.com>
2019-09-24 23:35:25 -0700

    Initial Commit

---
+++ tips/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Kelly von Borstel on 9/24/19.
+//  Copyright © 2019 Kelly von Borstel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
